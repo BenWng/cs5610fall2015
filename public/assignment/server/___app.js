4c8366aad31f3da3ae2e7f99d0d9efc47c7a39fd
Zhongxi Wang <Ben_Big@Big.local>
2015-11-29 21:58:01 -0500

    Remove unnecessary files

--- public/assignment/server/___app.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-
-    var userModel = require("./models/user.model.js")(app);
-    var formModel = require("./models/form.model.js")(app);
-
-    require("./services/user.service.js")(app, userModel, null);
-    require("./services/form.service.js")(app, formModel, null);
-    require("./services/field.service.js")(app, formModel, null);
-}

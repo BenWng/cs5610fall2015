75719ac54463770e69dd716f2a75a0404ad26167
Zhongxi Wang <Ben_Big@Big.local>
2015-12-21 18:21:30 -0500

    Change upload controller so user does not need to put in the common part in youtube url

--- public/project/client/view/Upload/Upload.controller.js
+++ public/project/client/view/Upload/Upload.controller.js
@@ -15,6 +15,7 @@ function uploadController($scope,$rootScope,$location,$routeParams,PostService,U
 		//$scope.post.author=$scope.currentUserName;
 		$scope.tagsArray=$scope.post.tags.split(',');
 		$scope.post.tags=$scope.tagsArray;
+		$scope.post.youtube="http://www.youtube.com/embed/"+$scope.post.youtube+"?autoplay=0";
 		for (var i=0; i<$scope.post.tags.length; i++){
 			$scope.post.tags[i]	= $scope.post.tags[i].replace(/(^\s+|\s+$)/g, '');
 		}
